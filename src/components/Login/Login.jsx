a2a14a3379ae8ec9066574a42852d2205b3f9736
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-01 16:36:16 +0600

    Enabled GitHub Authentication

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,15 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+  const [currentUser, setCurrentUser] = useState(null);
+
   const auth = getAuth(app);
   console.log(app);
   const provider = new GoogleAuthProvider();
@@ -10,16 +18,40 @@ const Login = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
-        console.log(user);
+        // console.log(user);
+        setCurrentUser(user);
       })
       .catch((error) => {
         console.log("Error", error.message);
       });
   };
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setCurrentUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
-      <button onClick={handleGoogleSignIn}>Google Login</button>
+      {currentUser ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google Login</button>
+      )}
+
+      {currentUser && (
+        <div>
+          <h3>User: {currentUser.displayName}</h3>
+          <p>Email: {currentUser.email}</p>
+          <img src={currentUser.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
